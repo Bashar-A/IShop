9b70a200f0a4402a123cb174a4073deabf88491b
Bashar Almamma <bashar@MBP-Maksim.mshome.net>
2021-02-08 13:23:23 +0300

    Updated attributes resolvers

--- modules/attributes/resolvers.js
+++ modules/attributes/resolvers.js
@@ -3,16 +3,33 @@ const Attribute = require('./model');
 async function Attributes(parent, args, context, info) {
     return await Attribute.find({})
 }
+
 async function addAttribute(parent, args, context, info) {
-    const arguments = context.variableValues
-    console.debug(args)
-    const attribute = await Attribute.Create(...arguments)
+    const input = context.variableValues.input
+    const attribute = await Attribute.create(input)
     await attribute.save()
+    return attribute
+}
+
+async function updateAttribute(parent, args, context, info) {
+    const input = context.variableValues.input
+    const attribute = await Attribute.findById(input.id)
+    attribute.set(input)
+    await attribute.save()
+    return attribute
+}
+
+async function deleteAttribute(parent, args, context, info) {
+    const id = context.variableValues.id
+    const attribute = await Attribute.findById(id)
+    await attribute.remove()
     return attribute
 }
 
 
 module.exports = {
     Attributes,
-    addAttribute
+    addAttribute,
+    updateAttribute,
+    deleteAttribute
   }
