495579a680f38afb7bfed6a8b71422dd3c4fa072
Bashar-A <60934125+Bashar-A@users.noreply.github.com>
2021-02-08 15:04:49 +0300

    Delete keys.dev.js

--- keys/keys.dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  DB_HOST: "cluster0.mb7rh.mongodb.net",
-  DB_NAME: "IShopDB",
-  DB_USER: "admin",
-  DB_PASSWORD: "1kKY8gOQ6b53Gx9l",
-  MONGODB_URI: 'mongodb+srv://admin:1kKY8gOQ6b53Gx9l@cluster0.mb7rh.mongodb.net/IShopDB',
-  BASE_URL: 'http://localhost',
-  JWT_SIGNATURE: "Secret"
-}
